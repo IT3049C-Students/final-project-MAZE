2b19d5d5fad3b2b3c5793c32b69a275575a3a6d0
dumforjh <dumforjh@mail.uc.edu>
2022-04-12 15:18:38 -0400

    started the maze file

---
+++ resources/maze.js
@@ -0,0 +1,144 @@
+let maze = document.querySelector('.maze');
+let ctx = maze.getContext('2d');
+let generationComplete = false;
+
+let current;
+let goal;
+
+class Maze {
+    constructor(size, rows, columns) {
+        this.size = size;
+        this.columns = columns;
+        this.rows = rows;
+        this.grid = [];
+        this.stack = [];
+    }
+
+    setup() {
+        for (let r = 0; r < this.rows; r++) {
+            let row = [];
+            for (let c = 0; c < this.columns; c++) {
+                let cell = new Cell(r, c, this.grid, this.size);
+                row.push(cell);
+            }
+            this.grid.push(row);
+        }
+        current = this.grid[0][0];
+        this.grid[this.rows - 1][this.columns - 1].goal = true;
+    }
+
+    draw() {
+        maze.width = this.size;
+        maze.height = this.size;
+        maze.style.background = "/resources/images/bg.png";
+        current.visited = true;
+        for (let r = 0; r < this.rows; r++) {
+            for (let c = 0; c < this.columns; c++) {
+                let grid = this.grid;
+                grid[r][c].show(this.size, this.rows, this.columns);
+            }
+        }
+        let next = current.checkNeighbors();
+        if (next) {
+            next.visited = true;
+            this.stack.push(current);
+            current.highlight(this.columns);
+            current.removeWalls(current, next);
+            current = next;
+        } else if (this.stack.length > 0) {
+            let cell = this.stack.pop();
+            current = cell;
+            current.highlight(this.columns);
+        }
+        if (this.stack.length === 0) {
+            generationComplete = true;
+            return;
+        }
+        window.requestAnimationFrame(() => {
+        this.draw();
+        });
+    }
+}
+
+class Cell {
+    constructor(rowNum, colNum, parentGrid, parentSize) {
+        this.rowNum = rowNum;
+        this.colNum = colNum;
+        this.parentGrid = parentGrid;
+        this.parentSize = parentSize;
+        this.walls = {
+            top: true,
+            right: true,
+            bottom: true,
+            left: true
+        };
+        this.goal = false;
+        this.visited = false;
+    }
+
+    checkNeighbors() {
+        let grid = this.parentGrid;
+        let row = this.rowNum;
+        let col = this.colNum;
+        let neighbors = [];
+
+        let top = row !== 0 ? grid[row - 1][col] : undefined;
+        let right = col !== grid[0].length - 1 ? grid[row][col + 1] : undefined;
+        let bottom = row !== grid.length - 1 ? grid[row + 1][col] : undefined;
+        let left = col !== 0 ? grid[row][col - 1] : undefined;
+
+        if (top && !top.visited) {
+            neighbors.push(top);
+        }
+        if (right && !right.visited) {
+            neighbors.push(right);
+        }
+        if (bottom && !bottom.visited) {
+            neighbors.push(bottom);
+        }
+        if (left && !left.visited) {
+            neighbors.push(left);
+        }
+        if (neighbors.length !== 0) {
+            let random = Math.floor(Math.random() * neighbors.length);
+            return neighbors[random];
+        } else {
+            return undefined;
+        }
+    }
+
+    drawTopWall(x, y, size, columns, rows) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x + size / columns, y);
+        ctx.stroke();
+    }
+
+    drawRightWall(x, y, size, columns, rows) {
+        ctx.beginPath();
+        ctx.moveTo(x + size / columns, y);
+        ctx.lineTo(x + size / columns, y + size / rows);
+        ctx.stroke();
+    }
+
+    drawBottomWall(x, y, size, columns, rows) {
+        ctx.beginPath();
+        ctx.moveTo(x, y + size / rows);
+        ctx.lineTo(x + size / columns, y + size / rows);
+        ctx.stroke();
+    }
+
+    drawLeftWall(x, y, size, columns, rows) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x, y + size / rows);
+        ctx.stroke();
+    }
+
+    highlight(columns) {
+        let x = (this.colNum * this.parentSize) / columns + 1;
+        let y = (this.rowNum * this.parentSize) / columns + 1;
+        ctx.fillStyle = "#ffbb00";
+        ctx.fillRect(x, y, this.parentSize / columns - 3, this.parentSize / columns - 3);
+    }
+}
