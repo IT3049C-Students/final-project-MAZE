b5ee3276337669a7379c7eeac70611ef4d99bd85
Kadam Patel <59839409+patel2kd@users.noreply.github.com>
2022-04-19 13:12:18 -0400

    Complete file

--- resources/settings.js
+++ resources/settings.js
@@ -1,8 +1,8 @@
 let form = document.querySelector("#controls");
 let size = document.querySelector("#size");
-let rowsCols = document.querySelector("#number");
-let complete = document.querySelector(".after");
-let replay = document.querySelector("#replay");
+let rc = document.querySelector("#number");
+let finish = document.querySelector(".after");
+let replay = document.querySelector(".replay");
 let close = document.querySelector(".quit");
 
 let newMaze;
@@ -10,77 +10,77 @@ let newMaze;
 form.addEventListener("submit", generateMaze);
 document.addEventListener("keydown", move);
 replay.addEventListener("click", () => {
-    location.reload();
+  location.reload();
 });
 
 close.addEventListener("click", () => {
-    complete.style.display = "none";
+  finish.style.display = "none";
 });
 
 function generateMaze(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    if (rowsCols.value == "" || size.value == "") {
-        return alert("The Crypt can't be built without everything!");
-    }
+  if (rc.value == "" || size.value == "") {
+    return alert("Please enter all fields");
+  }
 
-    let mazeSize = size.value;
-    let number = rowsCols.value;
-    if (mazeSize > 600 || number > 50) {
-        alert("This Crypt would be too big for you!");
-        return;
-    }
+  let mazeSize = size.value;
+  let number = rc.value;
+  if (mazeSize > 600 || number > 50) {
+    alert("Maze too large!");
+    return;
+  }
 
-    form.style.display = "none";
+  form.style.display = "none";
 
-    newMaze = new Maze(mazeSize, number, number);
-    newMaze.setup();
-    newMaze.draw();
+  newMaze = new MazeRunner(mazeSize, number, number);
+  newMaze.setup();
+  newMaze.draw();
 }
 
 function move(e) {
-    if (!generationComplete) return;
-    let key = e.key;
-    let row = current.rowNum;
-    let col = current.colNum;
+  if (!generationComplete) return;
+  let key = e.key;
+  let row = current.rowNum;
+  let col = current.colNum;
 
-    switch (key) {
-        case "ArrowUp":
-            if (!current.walls.topWall) {
-                let next = newMaze.grid[row - 1][col];
-                current = next;
-                newMaze.draw();
-                current.highlight(newMaze.columns);
-            }
-            break;
+  switch (key) {
+    case "ArrowUp":
+      if (!current.walls.topWall) {
+        let next = newMaze.grid[row - 1][col];
+        current = next;
+        newMaze.draw();
+        current.highlight(newMaze.columns);
+      }
+      break;
 
-        case "ArrowDown":
-            if (!current.walls.bottomWall) {
-                let next = newMaze.grid[row + 1][col];
-                current = next;
-                newMaze.draw();
-                current.highlight(newMaze.columns);
-                if (current.goal) complete.style.display = "block";
-            }
-            break;
-        
-        case "ArrowLeft":
-            if (!current.walls.leftWall) {
-                let next = newMaze.grid[row][col - 1];
-                current = next;
-                newMaze.draw();
-                current.highlight(newMaze.columns);
-            }
-            break;
+    case "ArrowRight":
+      if (!current.walls.rightWall) {
+        let next = newMaze.grid[row][col + 1];
+        current = next;
+        newMaze.draw();
+        current.highlight(newMaze.columns);
+        if (current.goal) finish.style.display = "block";
+      }
+      break;
 
-        case "ArrowRight":
-            if (!current.walls.rightWall) {
-                let next = newMaze.grid[row][col + 1];
-                current = next;
-                newMaze.draw();
-                current.highlight(newMaze.columns);
-                if (current.goal) complete.style.display = "block";
-            }
-            break;
-    }
+    case "ArrowDown":
+      if (!current.walls.bottomWall) {
+        let next = newMaze.grid[row + 1][col];
+        current = next;
+        newMaze.draw();
+        current.highlight(newMaze.columns);
+        if (current.goal) finish.style.display = "block";
+      }
+      break;
+
+    case "ArrowLeft":
+      if (!current.walls.leftWall) {
+        let next = newMaze.grid[row][col - 1];
+        current = next;
+        newMaze.draw();
+        current.highlight(newMaze.columns);
+      }
+      break;
+  }
 }
