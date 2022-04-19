83bc833b90ba1966401dbd47fafce1e6591c6cf9
dumforjh <dumforjh@mail.uc.edu>
2022-04-19 14:46:29 -0400

    added comments

--- maze.js
+++ maze.js
@@ -14,75 +14,58 @@ class MazeRunner {
     this.stack = [];
   }
 
-  // Set the grid: Create new this.grid array based on number of instance rows and columns
+  // This makes the grid.
   setup() {
     for (let r = 0; r < this.rows; r++) {
       let row = [];
       for (let c = 0; c < this.columns; c++) {
-        // Create a new instance of the Cell class for each element in the 2D array and push to the maze grid array
+        
         let cell = new Cell(r, c, this.grid, this.size);
         row.push(cell);
       }
       this.grid.push(row);
     }
-    // Set the starting grid
     current = this.grid[0][0];
     this.grid[this.rows - 1][this.columns - 1].goal = true;
   }
 
-  // Draw the canvas by setting the size and placing the cells in the grid array on the canvas.
+  // This draws the grid the maze is gonna be on.
   draw() {
     m.width = this.size;
     m.height = this.size;
     m.style.background = "black";
-    // Set the first cell as visited
     current.visited = true;
-    // Loop through the 2d grid array and call the show method for each cell instance
     for (let r = 0; r < this.rows; r++) {
       for (let c = 0; c < this.columns; c++) {
         let grid = this.grid;
         grid[r][c].show(this.size, this.rows, this.columns);
       }
     }
-    // This function will assign the variable 'next' to random cell out of the current cells available neighbouting cells
     let next = current.checkNeighbours();
-    // If there is a non visited neighbour cell
+    
     if (next) {
       next.visited = true;
-      // Add the current cell to the stack for backtracking
       this.stack.push(current);
-      // this function will highlight the current cell on the grid. The parameter columns is passed
-      // in order to set the size of the cell
       current.highlight(this.columns);
-      // This function compares the current cell to the next cell and removes the relevant walls for each cell
       current.removeWalls(current, next);
-      // Set the nect cell to the current cell
       current = next;
-
-      // Else if there are no available neighbours start backtracking using the stack
     } else if (this.stack.length > 0) {
       let cell = this.stack.pop();
       current = cell;
       current.highlight(this.columns);
     }
-    // If no more items in the stack then all cells have been visted and the function can be exited
     if (this.stack.length === 0) {
       generationComplete = true;
       return;
     }
 
-    // Recursively call the draw function. This will be called up until the stack is empty
     window.requestAnimationFrame(() => {
       this.draw();
     });
-    //     setTimeout(() => {rd
-    //       this.draw();
-    //     }, 10);
   }
 }
 
 class Cell {
-  // Constructor takes in the rowNum and colNum which will be used as coordinates to draw on the canvas.
   constructor(rowNum, colNum, parentGrid, parentSize) {
     this.rowNum = rowNum;
     this.colNum = colNum;
@@ -94,8 +77,6 @@ class Cell {
       leftWall: true,
     };
     this.goal = false;
-    // parentGrid is passed in to enable the checkneighbours method.
-    // parentSize is passed in to set the size of each cell on the grid
     this.parentGrid = parentGrid;
     this.parentSize = parentSize;
   }
@@ -106,20 +87,19 @@ class Cell {
     let col = this.colNum;
     let neighbours = [];
 
-    // The following lines push all available neighbours to the neighbours array
-    // undefined is returned where the index is out of bounds (edge cases)
+    
     let top = row !== 0 ? grid[row - 1][col] : undefined;
     let right = col !== grid.length - 1 ? grid[row][col + 1] : undefined;
     let bottom = row !== grid.length - 1 ? grid[row + 1][col] : undefined;
     let left = col !== 0 ? grid[row][col - 1] : undefined;
 
-    // if the following are not 'undefined' then push them to the neighbours array
+    
     if (top && !top.visited) neighbours.push(top);
     if (right && !right.visited) neighbours.push(right);
     if (bottom && !bottom.visited) neighbours.push(bottom);
     if (left && !left.visited) neighbours.push(left);
 
-    // Choose a random neighbour from the neighbours array
+    
     if (neighbours.length !== 0) {
       let random = Math.floor(Math.random() * neighbours.length);
       return neighbours[random];
@@ -128,7 +108,7 @@ class Cell {
     }
   }
 
-  // Wall drawing functions for each cell. Will be called if relevent wall is set to true in cell constructor
+  // Draws the walls of the maze.
   drawTopWall(x, y, size, columns, rows) {
     ctx.beginPath();
     ctx.moveTo(x, y);
@@ -157,9 +137,8 @@ class Cell {
     ctx.stroke();
   }
 
-  // Highlights the current cell on the grid. Columns is once again passed in to set the size of the grid.
+  // Highlights the player position on the grid.
   highlight(columns) {
-    // Additions and subtractions added so the highlighted cell does cover the walls
     let x = (this.colNum * this.parentSize) / columns + 1;
     let y = (this.rowNum * this.parentSize) / columns + 1;
     ctx.fillStyle = "Red";
@@ -172,9 +151,7 @@ class Cell {
   }
 
   removeWalls(cell1, cell2) {
-    // compares to two cells on x axis
     let x = cell1.colNum - cell2.colNum;
-    // Removes the relevant walls if there is a different on x axis
     if (x === 1) {
       cell1.walls.leftWall = false;
       cell2.walls.rightWall = false;
@@ -182,9 +159,7 @@ class Cell {
       cell1.walls.rightWall = false;
       cell2.walls.leftWall = false;
     }
-    // compares to two cells on x axis
     let y = cell1.rowNum - cell2.rowNum;
-    // Removes the relevant walls if there is a different on x axis
     if (y === 1) {
       cell1.walls.topWall = false;
       cell2.walls.bottomWall = false;
